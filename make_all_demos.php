6fa726ffb35995a7f78ee21b6907ddc6b22573c7
rw <rw@20sec.net>
2026-04-27 14:12:37 +0200

    make script comments

--- make_all_demos.php
+++ make_all_demos.php
@@ -1,20 +1,23 @@
 <?php
+// this script builds the github demo page
 
-// print_r(PHP_BINARY);
-// print_r($argv);
+// all pages live in this subdir
 $prefix = "/slowfoot-examples";
 
-$command = "%s vendor/bin/slowfoot build -f -p=%s";
-
-$cmd = sprintf($command, PHP_BINARY, $prefix);
+// at first, we build the index page 
+// from the README.md file, without config
+$cmd = sprintf("%s vendor/bin/slowfoot build -f -p=%s", PHP_BINARY, $prefix);
 print "$cmd\n";
 shell_exec($cmd);
 
+// now we build all projects from the subdirectories
 $demos = ["take-me-baby"];
 foreach ($demos as $demo) {
     $pfx = $prefix . "/$demo";
     $cmd = sprintf("%s vendor/bin/slowfoot build -f -d=%s -p=%s", PHP_BINARY, $demo, $pfx);
     print "$cmd\n";
     shell_exec($cmd);
+
+    // after build, we move the output to /dist
     shell_exec("mv $demo/dist dist/$demo");
 }
